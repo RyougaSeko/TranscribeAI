e8aeac24a93ab01134c00bdaaf3688aba0f7fcea
hoop105ryouga <hoop105ryouga@icloud.com>
2023-01-07 18:26:44 +0900

    commit

--- app.py
+++ app.py
@@ -112,6 +112,3 @@ def login():
         return render_template('login.html')
     else:
         return render_template('login.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
